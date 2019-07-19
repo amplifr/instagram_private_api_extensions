65ffda0dea88de6b309a2947bb642370b476fe06
Alex Gaziev <alex.gaziev@gmail.com>
2019-07-19 16:19:33 -0700

    Remove requirement for moviepy

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setup(
     keywords='instagram private api extensions',
     description='An extension module for https://github.com/ping/instagram_private_api',
     packages=packages,
-    install_requires=['git+https://git@github.com/amplifr/moviepy.git@8bd1a76aa723a13e8ce92875eda4abe032a425f0', 'Pillow>=4.0.0', 'requests>=2.9.1'],
+    install_requires=['Pillow>=4.0.0', 'requests>=2.9.1'],
     test_requires=test_reqs,
     classifiers=[
         'Development Status :: 4 - Beta',
